80c4ad72967f02f6f19c547b836e57959627cf7a
Vishal27-com <vishalgupta962148@gmail.com>
2023-04-16 21:02:45 +0530

    Provider added

--- client/number-game/src/main.jsx
+++ client/number-game/src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import { store } from "./Redux/store";
+import {Provider} from 'react-redux'
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
       <BrowserRouter>
+      <Provider store={store}>
         <App />
+      </Provider>
       </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
